df90b4008f5f48858ae3d34dbd3bef7d2bb811d2
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-09 20:07:52 -0500

    migration file of the users, full schema

---
+++ database/migrations/2016_07_10_010118_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('fist_name', 45);
+            $table->string('last_name', 45);
+            $table->string('business_name', 45);
+            $table->string('identification', 45);
+            $table->string('contact', 45);
+            $table->string('email', 100);
+            $table->string('address', 45);
+            $table->integer('city_id')->unsigned();
+            $table->foreign('city_id')->reference('id')->on('cities');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->reference('id')->on('states');
+            $table->string('picture', 255);
+            $table->string('username', 45);
+            $table->string('password', 45);
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')->reference('id')->on('roles');
+            $table->boolean('status')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}

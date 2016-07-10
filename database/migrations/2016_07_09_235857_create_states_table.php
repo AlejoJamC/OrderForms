39b600e0504a15fcc2a2adffd2b962d7cfbc6b3c
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-09 19:44:50 -0500

    migration of states, full schema

---
+++ database/migrations/2016_07_09_235857_create_states_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name', 45);
+            $table->boolean('status')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('states');
+    }
+}

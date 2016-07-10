f611cbe30c5d83e1cbdb40c429b2c9bf483427b8
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-09 20:54:20 -0500

    setup the seeders inside run method from database seeder entity  [TODO: avoid only spanish content]

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+        $this->call(OrderStatesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
     }
 }

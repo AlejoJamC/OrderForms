5c391334a7ab1854a5bf8b6a28852cae17f2dea0
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-09 19:58:11 -0500

    migration file of products, full schema

---
+++ database/migrations/2016_07_10_005510_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('title', 45);
+            $table->string('description', 200);
+            $table->string('presentation', 45);
+            $table->string('brand', 45);
+            $table->string('reference', 45);
+            $table->float('price');
+            $table->float('price_with_tax');
+            $table->integer('quantity')->unsigned();
+            $table->boolean('status')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}

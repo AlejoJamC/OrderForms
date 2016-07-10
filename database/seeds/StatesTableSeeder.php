c79e8802e3b025500ddde3fe525440e080fc0a79
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-10 14:40:29 -0500

    static content for states of Colombia and fixed error with git corrupt

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -11,6 +11,17 @@ class StatesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // TODO: avoid only spanish seeds
+        $states = ["Amazonas", "Antioquia", "Arauca", "Atlantico", "Bogota", "Bolivar",
+            "Boyaca", "Caldas", "Caqueta", "Casanare", "Cauca", "Cesar", "Choco", "Cordoba",
+            "Cundinamarca", "Guainia", "Guaviare", "Huila", "La Guajira", "Magdalena", "Meta",
+            "Narino", "Norte de Santander", "Putumayo", "Quindio", "Risaralda", "San Andres y Providencia",
+            "Santander", "Sucre", "Tolima", "Valle del Cauca", "Vaupes", "Vichada"];
+
+        array_map(function ($name){
+            DB::table('roles')->insert([
+                'name' => $name,
+            ]);
+        }, $states);
     }
 }

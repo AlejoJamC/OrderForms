0eccad8334599d17c8f01ec7c22f5b49591ac72f
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-11 01:29:34 -0500

    partials used by main layout [TODO: fix footer not showing problem]

---
+++ resources/views/partials/main/footer.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main')
+@section('footer')
+<div class="login-footer">
+    <div class="row bs-reset">
+        <div class="col-xs-5 bs-reset">
+            <ul class="login-social">
+                <li>
+                    <a href="javascript:;">
+                        <i class="icon-social-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:;">
+                        <i class="icon-social-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="javascript:;">
+                        <i class="icon-social-dribbble"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-xs-7 bs-reset">
+            <div class="login-copyright text-right">
+                <p>Copyright &copy; Unicontacto</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

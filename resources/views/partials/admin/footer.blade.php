bc8c3137763cd937faf62d1c4b1a3d1b758cb45e
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-11 03:05:24 -0500

    footer partial of admin layout

---
+++ resources/views/partials/admin/footer.blade.php
@@ -0,0 +1,10 @@
+@section('footer')
+<div class="page-footer">
+    <div class="page-footer-inner">
+        Copyright &copy; Unicontacto {{ date("Y") }}
+    </div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+@show

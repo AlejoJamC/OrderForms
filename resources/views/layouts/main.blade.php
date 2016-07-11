c6912db0354f0ba0791d6336eaf9b662ec857314
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-11 01:28:30 -0500

    create the main layouts for all application,"main" for login and no session pages and "admin" to manage

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+@include('partials.main.head')
+<!-- END HEAD -->
+<body class=" login">
+<!-- BEGIN LOGIN -->
+@yield('content')
+<!-- END LOGIN -->
+<!-- BEGIN SCRIPTS -->
+@include('partials.main.scripts')
+<!-- END SCRIPTS -->
+</body>
+
+</html>

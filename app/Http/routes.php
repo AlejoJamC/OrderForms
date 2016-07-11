ea503585252ca02c00172deda74f55c4c823d8d9
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-11 18:26:57 -0500

    reorder of routes, all TODOs from views was writing here

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,6 +12,9 @@
 */
 
 // TODO: Avoid only spanish routes
+// TODO: Improve the use of nouns and reorder the routes
+// TODO: Fix the problem with Request:is('route') to show on the same level
+// TODO: Build a helper to manage active menu 
 
 /*
  * Routes for Login in this case is the entry point or index
@@ -23,20 +26,43 @@ Route::get('/home', 'HomeController@index');
 /*
  * Routes for history
  */
-Route::get('/dash','HistoryController@index');
-Route::get('/dash/history','HistoryController@index');
+Route::get('/dash','HistoryController@redirectTo');
+Route::get('/dash/history','HistoryController@redirectTo');
 Route::get('/dash/history/me','HistoryController@index');
-Route::get('/dash/history/all','HistoryController@fullHistory');
+Route::get('/dash/history/all','HistoryController@listAll');
+
+/*
+ * Routes for Orders
+ */
+Route::get('/dash/orders','OrdersController@index');
+Route::get('/dash/orders/new','OrdersController@index');
+Route::get('/dash/orders/update','OrdersController@index');
+
+/*
+ * Routes for Products
+ */
+Route::get('/dash/products','ProductsController@index');
+Route::get('/dash/products/list','ProductsController@index');
+Route::get('/dash/products/update','ProductsController@index');
+Route::get('/dash/products/load','ProductsController@index');
+
+/*
+ * Routes for Users
+ */
+Route::get('/dash/users','UsersController@index');
+Route::get('/dash/users/list','UsersController@index');
+Route::get('/dash/users/new','UsersController@index');
+Route::get('/dash/users/reset/password','UsersController@index');
 
 /*
  * Routes for States
  */
-Route::get('/dash/departamento','StateController@index');
+Route::get('/dash/states','StateController@index');
 
 /*
  * Routes for Cities
  */
-Route::get('/dash/ciudades','CitiesController@index');
+Route::get('/dash/cities','CitiesController@index');
 
 /*
  * Routes for Roles
@@ -46,24 +72,9 @@ Route::get('/dash/roles','RolesController@index');
 /*
  * Routes for Order states
  */
-Route::get('/dash/estados/ordenes','OrderStatesController@index');
-
-/*
- * Routes for Products
- */
-Route::get('/dash/productos','ProductsController@index');
-
-/*
- * Routes for Users
- */
-Route::get('/dash/usuarios','UsersController@index');
-
-/*
- * Routes for Orders
- */
-Route::get('/dash/ordenes','OrdersController@index');
+Route::get('/dash/states/orders','OrderStatesController@index');
 
 /*
  * Routes for Order Details
  */
-Route::get('/dash/orden/detalle','OrderDetailController@index');
+Route::get('/dash/orders/details','OrderDetailController@index');

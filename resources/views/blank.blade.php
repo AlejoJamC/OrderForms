45df062020155f6f02bf1e2c2dcbcaf7762e9bfd
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-11 18:29:22 -0500

    new temporal view to show something while the html and css is selected for every route

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+@section('css')
+@stop
+
+@section('title', 'Historial de las ordenes de compra')
+
+@section('content')
+    <div class="page-content-wrapper">
+        <!-- BEGIN CONTENT BODY -->
+        <div class="page-content">
+            <!-- BEGIN PAGE HEADER-->
+            <h3 class="page-title"> Blank Page
+                <small>Nothing to do here.</small>
+            </h3>
+            <div class="page-bar">
+                <ul class="page-breadcrumb">
+                    <li>
+                        <i class="icon-home"></i>
+                        <a href="/dash">Home</a>
+                        <i class="fa fa-angle-right"></i>
+                    </li>
+                    <li>
+                        <span>Blank Page</span>
+                    </li>
+                </ul>
+                <div class="page-toolbar">
+                    <div class="btn-group pull-right">
+                        <button type="button" class="btn btn-fit-height grey-salt dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="1000" data-close-others="true"> Actions
+                            <i class="fa fa-angle-down"></i>
+                        </button>
+                        <ul class="dropdown-menu pull-right" role="menu">
+                            <li>
+                                <a href="#">
+                                    <i class="icon-bell"></i> Action</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="icon-shield"></i> Another action</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="icon-user"></i> Something else here</a>
+                            </li>
+                            <li class="divider"> </li>
+                            <li>
+                                <a href="#">
+                                    <i class="icon-bag"></i> Separated link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- END PAGE HEADER-->
+        </div>
+        <!-- END CONTENT BODY -->
+    </div>
+@endsection
+
+@section('js')
+@endsection

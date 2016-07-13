a140d437b59a3917fc75e682e02d40745cd97168
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-13 15:21:40 -0500

    updating html tables

--- resources/views/history/all.blade.php
+++ resources/views/history/all.blade.php
@@ -41,28 +41,16 @@
             <!-- BEGIN AJAX DATATABLE-->
             <div class="row">
                 <div class="col-md-12">
-                    <!-- Begin: life time stats -->
-                    <div class="portlet light portlet-fit portlet-datatable ">
+                    <div class="portlet light ">
                         <div class="portlet-title">
                             <div class="caption">
-                                <i class="icon-settings font-dark"></i>
-                                <span class="caption-subject font-dark sbold uppercase">Todas las ordenes de todos los usuarios</span>
+                                <i class="icon-social-dribbble font-green"></i>
+                                <span class="caption-subject font-green bold uppercase">Todas las ordenes de todos los usuarios</span>
                             </div>
                         </div>
                         <div class="portlet-body">
-                            <div class="table-container">
-                                <div class="table-actions-wrapper">
-                                    <span> </span>
-                                    <select class="table-group-action-input form-control input-inline input-small input-sm">
-                                        <option value="">Seleccionar...</option>
-                                        @foreach ($order_states as $order_states_id => $order_state)
-                                            <option value="{{ $order_states_id }}">{{ $order_state }}</option>
-                                        @endforeach
-                                    </select>
-                                    <button class="btn btn-sm green table-group-action-submit">
-                                        <i class="fa fa-check"></i> Filtrar</button>
-                                </div>
-                                <table class="table table-striped table-bordered table-hover table-checkable" id="datatable_ajax">
+                            <div class="table-scrollable">
+                                <table class="table table-hover" id="tbhistoy" name="tbhistoy">
                                     <thead>
                                     <tr role="row" class="heading">
                                         <th width="1%"></th>
@@ -72,74 +60,25 @@
                                         <th width="15%"> Estado </th>
                                         <th width="15%"> Acciones </th>
                                     </tr>
-                                    <tr role="row" class="filter">
-                                        <td> </td>
+                                    </thead>
+                                    <tbody id="tbodyhistory">
+                                    <tr role="row" class="odd">
+                                        <td></td>
+                                        <td class="sorting_1">1</td>
+                                        <td>Jhon Doe</td>
+                                        <td>12/09/2013</td>
                                         <td>
-                                            <input type="text" class="form-control form-filter input-sm" name="order_id">
+                                            <span class="label label-sm label-success">Pending</span>
                                         </td>
                                         <td>
-                                            <input type="text" class="form-control form-filter input-sm" name="order_customer_name">
-                                        </td>
-                                        <td>
-                                            <div class="input-group date date-picker margin-bottom-5" data-date-format="dd/mm/yyyy">
-                                                <input type="text" class="form-control form-filter input-sm" readonly name="order_date_from" placeholder="From">
-                                                    <span class="input-group-btn">
-                                                        <button class="btn btn-sm default" type="button">
-                                                            <i class="fa fa-calendar"></i>
-                                                        </button>
-                                                    </span>
-                                            </div>
-                                            <div class="input-group date date-picker" data-date-format="dd/mm/yyyy">
-                                                <input type="text" class="form-control form-filter input-sm" readonly name="order_date_to" placeholder="To">
-                                                    <span class="input-group-btn">
-                                                        <button class="btn btn-sm default" type="button">
-                                                            <i class="fa fa-calendar"></i>
-                                                        </button>
-                                                    </span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <select name="order_status" class="form-control form-filter input-sm">
-                                                <option value="">Seleccionar...</option>
-                                                @foreach ($order_states as $order_states_id => $order_state)
-                                                    <option value="{{ $order_states_id }}">{{ $order_state }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <div class="margin-bottom-5">
-                                                <button class="btn btn-sm green btn-outline filter-submit margin-bottom">
-                                                    <i class="fa fa-search"></i> Filtrar</button>
-                                            </div>
-                                            <button class="btn btn-sm red btn-outline filter-cancel">
-                                                <i class="fa fa-times"></i> Reiniciar</button>
+                                            <a href="javascript:;" class="btn btn-sm btn-outline grey-salsa"><i class="fa fa-search"></i> Ver detalle</a>
                                         </td>
                                     </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr role="row" class="odd">
-                                            <td>
-                                                <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
-                                                    <input name="id[]" type="checkbox" class="checkboxes" value="1">
-                                                    <span></span>
-                                                </label>
-                                            </td>
-                                            <td class="sorting_1">1</td>
-                                            <td>Jhon Doe</td>
-                                            <td>12/09/2013</td>
-                                            <td>
-                                                <span class="label label-sm label-success">Pending</span>
-                                            </td>
-                                            <td>
-                                                <a href="javascript:;" class="btn btn-sm btn-outline grey-salsa"><i class="fa fa-search"></i> View</a>
-                                            </td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
-                    <!-- End: life time stats -->
                 </div>
             </div>
             <!-- END AJAX DATATABLE-->
@@ -161,6 +100,37 @@
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN PAGE LEVEL SCRIPTS -->
     <script src="{{ URL::asset('assets/pages/scripts/dashboard.min.js') }}" type="text/javascript"></script>
-    <script src="{{ URL::asset('js/table-datatables-ajax-custom.js') }}" type="text/javascript"></script>
     <!-- END PAGE LEVEL SCRIPTS -->
+    <script>
+        $('#state').on('change', function(e){
+            console.log(e);
+            var state_id = e.target.value;
+
+            $.get('{{ url('dash') }}/history/ajax-all?user_id=' + user_id, function(data) {
+                console.log(data);
+                $('#tbhistoy').empty();
+                $('#tbhistoy').append($('<option>', {
+                    value: 0,
+                    text : "Seleccionar..."
+                }));
+                $.each(data, function(index,cities){
+                    $('#city').append($('<option>', {
+                        value: cities.id,
+                        text : cities.name
+                    }));
+                });
+            });
+        });
+
+        jQuery(document).ready(function() {
+            $.get('{{ url('dash') }}/history/ajax-all', function(data) {
+                console.log(data);
+                $('#tbodyhistory').empty();
+                $.each(data, function(index,histories){
+
+                });
+            });
+        });
+
+    </script>
 @endsection

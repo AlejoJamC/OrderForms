fcc831d8e5d53979132444fc8da6d0ed40f79b5f
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-14 19:47:55 -0500

    validator for register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -56,9 +56,8 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
+            'username' => 'required|max:255',
+            'password' => 'required|min:5',
         ]);
     }
 

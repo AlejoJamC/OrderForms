8f36b75c02b649355a72bd5bc7675a4a05136ff2
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-16 05:55:43 -0500

    fixed the size of the float

--- database/migrations/2016_07_10_005510_create_products_table.php
+++ database/migrations/2016_07_10_005510_create_products_table.php
@@ -17,9 +17,9 @@ class CreateProductsTable extends Migration
             $table->string('title', 255);
             $table->string('presentation', 255)->nullable();
             $table->string('brand', 255)->nullable();
-            $table->float('price');
-            $table->float('price_with_tax')->nullable();
-            $table->integer('quantity')->unsigned();
+            $table->float('price', '15', '2');
+            $table->float('price_with_tax','15', '2')->nullable();
+            $table->integer('quantity')->unsigned()->nullable();
             $table->boolean('status')->default(1);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
